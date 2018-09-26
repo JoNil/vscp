b6b9d9125fb90ae5e2e6ba4b389c7df3e490c002
Jonathan Nilsson <jonathan@voysys.se>
2018-09-26 20:34:26 +0200

    Translate from python

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,98 @@ extern crate i2cdev;
 
 use std::thread;
 use std::time::Duration;
-
-use i2cdev::core::*;
 use i2cdev::linux::LinuxI2CDevice;
+use math::round::floor;
 
-const PCA_9685_ADDR: u16 = 0x40;
+const PCA9685_ADDRESS: u16 = 0x40;
+const MODE1: u8            = 0x00;
+const MODE2: u8            = 0x01;
+const SUBADR1: u8          = 0x02;
+const SUBADR2: u8          = 0x03;
+const SUBADR3: u8          = 0x04;
+const PRESCALE: u8         = 0xFE;
+const LED0_ON_L: u8        = 0x06;
+const LED0_ON_H: u8        = 0x07;
+const LED0_OFF_L: u8       = 0x08;
+const LED0_OFF_H: u8       = 0x09;
+const ALL_LED_ON_L: u8     = 0xFA;
+const ALL_LED_ON_H: u8     = 0xFB;
+const ALL_LED_OFF_L: u8    = 0xFC;
+const ALL_LED_OFF_H: u8    = 0xFD;
+
+const RESTART: u8          = 0x80;
+const SLEEP: u8            = 0x10;
+const ALLCALL: u8          = 0x01;
+const INVRT: u8            = 0x10;
+const OUTDRV: u8           = 0x04;
+
+fn set_pwm_freq(dev: &mut LinuxI2CDevice, freq_hz: u16) {
+    let mut prescaleval = 25000000.0;
+    prescaleval /= 4096.0;
+    prescaleval /= freq_hz as f64;
+    prescaleval -= 1.0;
+
+    let prescale = floor(prescaleval + 0.5, 0) as u16;
+    let oldmode = dev.smbus_read_byte_data(MODE1);
+    let newmode = (oldmode & 0x7F) | 0x10;
+
+    dev.smbus_write_byte_data(MODE1, newmode);
+    dev.smbus_write_byte_data(PRESCALE, prescale);
+    dev.smbus_write_byte_data(MODE1, oldmode);
+    
+    thread::sleep(Duration::from_millis(50));
+
+    dev.smbus_write_byte_data(MODE1, oldmode | 0x80);
+}
+
+fn set_pwm(dev: &mut LinuxI2CDevice, channel: u16, on: u16, off: u16) {
+    
+    dev.smbus_write_byte_data(LED0_ON_L+4*channel,  on & 0xFF);
+    dev.smbus_write_byte_data(LED0_ON_H+4*channel,  on >> 8);
+    dev.smbus_write_byte_data(LED0_OFF_L+4*channel, off & 0xFF);
+    dev.smbus_write_byte_data(LED0_OFF_H+4*channel, off >> 8);
+}
+
+fn set_all_pwm(dev: &mut LinuxI2CDevice, on: u16, off: u16): {
+    
+    dev.smbus_write_byte_data(ALL_LED_ON_L,  on & 0xFF);
+    dev.smbus_write_byte_data(ALL_LED_ON_H,  on >> 8);
+    dev.smbus_write_byte_data(ALL_LED_OFF_L, off & 0xFF);
+	dev.smbus_write_byte_data(ALL_LED_OFF_H, off >> 8);
+}
 
 fn main() {
-    let mut dev = (LinuxI2CDevice::new("/dev/i2c-1", PCA_9685_ADDR)).expect("1");
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", PCA9685_ADDRESS).expect("1");
 
-    (dev.smbus_write_byte_data(0xF0, 0x55)).expect("2");
-    (dev.smbus_write_byte_data(0xFB, 0x00)).expect("3");
+    set_all_pwm(&mut dev, 0, 0);
+    dev.smbus_write_byte_data(MODE2, OUTDRV);
+    dev.smbus_write_byte_data(MODE1, ALLCALL);
+    
+	thread::sleep(Duration::from_millis(50));
 
-    thread::sleep(Duration::from_millis(100));
+    let mode1 = dev.smbus_read_byte_data(MODE1);
+    mode1 = mode1 & ~SLEEP;
+    dev.smbus_write_byte_data(MODE1, mode1)
+
+	thread::sleep(Duration::from_millis(50));
+
+
+	set_pwm_freq(&mut dev, 60);
+
+	loop {
+
+		let servo_min = 150;
+		let servo_max = 600;
+
+		set_pwm(&mut dev, 0, 0, servo_min);
+		set_pwm(&mut dev, 1, 0, servo_min);
+
+		thread::sleep(Duration::from_millis(1000));
+
+		set_pwm(&mut dev, 0, 0, servo_max);
+		set_pwm(&mut dev, 1, 0, servo_max);
+
+		thread::sleep(Duration::from_millis(1000));
+	}
+
 }
